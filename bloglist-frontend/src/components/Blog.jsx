9c82da96f7e629438b746d719a815c81e920ca56
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-07-03 23:43:15 +0200

    es 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleClick = () => {
+    let res = !visible;
+    setVisible(res);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={handleClick}>view</button>
+      </div>
+      {visible ? (
+        <>
+          <p>{blog.url}</p>
+          <p>likes: {blog.likes}</p>
+          <button>like</button>
+          {blog.user && <p>{blog.user.name} </p>}
+          // some old data in the db doesnt have user info in blogs
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+export default Blog;
