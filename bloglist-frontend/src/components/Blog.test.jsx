398f1bc8bd0fdb1104fce8e7be6195cf78455b3f
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-07-04 22:15:09 +0200

    es 5.14

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders content", () => {
@@ -31,4 +32,37 @@ test("renders content", () => {
   expect(likes).not.toBeInTheDocument();
   expect(user).not.toBeInTheDocument();
   // screen.debug();
+});
+
+test("clicking the button calls event handler once", async () => {
+  const blog = {
+    title: "test name",
+    author: "max",
+    likes: 14,
+    user: {
+      name: "Superuser",
+    },
+  };
+  // PropTypes causes error without functions
+  const mockIncreaseLikes = vi.fn();
+  const mockDeleteBlog = vi.fn();
+
+  render(
+    <Blog
+      blog={blog}
+      increaseLikes={mockIncreaseLikes}
+      deleteBlog={mockDeleteBlog}
+    />
+  );
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+  screen.debug();
+  // no need for mock function as its not a prop, clicking button makes likes and user visible
+  //expect(mockHandler.mock.calls).toHaveLength(1);
+  const likes = screen.getByText("likes: 14");
+  const userName = screen.getByText("Superuser");
+  expect(likes).toBeDefined();
+  expect(userName).toBeDefined();
 });
